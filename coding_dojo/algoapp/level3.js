a33b0b623ca50cf847bb073da7e45dde451cd41d
Jim Yoon <jimyoon90@gmail.com>
2018-03-26 15:54:17 -0700

    finished lvl 3 algo app

--- coding_dojo/algoapp/level3.js
+++ coding_dojo/algoapp/level3.js
@@ -50,16 +50,40 @@ x[x.length - 1] = x[0];
 console.log(x); // Result: [1, 3, 5, 8, 2, -5, 1]
 
 // Challenge 9
-
+var x = [1, 3, 5, 8, 2, -5, -8];
+var temp = x[x.length - 1];
+x[x.length - 1] = x[0];
+x[0] = temp;
+console.log(x); // Result: [-8, 3, 5, 8, 2, -5, 1]
 
 // Challenge 10
+var x = [1, 3, 5];
+x.push(7);
+console.log(x); // Result: [1, 3, 5, 7]
 
 // Challenge 11
+var x = [2, 4, 6];
+x.push(3);
+console.log(x); // [2, 4, 6, 3]
 
 // Challenge 12
+var x = [1, 3, 5];
+var y = x[0] + x[1];
+x.push(y);
+console.log(x); // Result: [1, 3, 5, 4]
 
 // Challenge 13
+var x = [2, 3, 5];
+x.push(x[0] + x[1]);
+console.log(x); // Result: [2, 3, 5, 5]
 
 // Challenge 14
+var x = [1, 3, 5];
+x.pop();
+console.log(x); // Result: [1, 3]
 
 // Challenge 15
+var x = [1, 3, 5];
+x.push(7);
+x.pop();
+console.log(x); // Result: [1, 3, 5]
