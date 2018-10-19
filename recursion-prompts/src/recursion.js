4fcc641bdb93d6c4b71f2742436b2c29646885f1
Jim Yoon <jimyoon90@gmail.com>
2018-10-19 01:37:22 -0700

    solved 2 recursion problems

--- recursion-prompts/src/recursion.js
+++ recursion-prompts/src/recursion.js
@@ -6,12 +6,23 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
-var factorial = function(n) {
+const factorial = (n) => {
+  // base case
+  if (n === 0) {
+    return 1;
+  }
+  // recursive case
+  return n * factorial (n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
-var sum = function(array) {
+const sum = (array) => {
+  // base case
+  if (array.length === 1) {
+    return array[0];
+  }
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
